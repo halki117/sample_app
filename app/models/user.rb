fbe4d07282ef9183f90b28072100422858ac369e
halki <s.halki0203@icloud.com>
2020-07-11 11:10:03 +0900

    up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: true
-  has_secure_password
+  # has_secure_password
 end
  
