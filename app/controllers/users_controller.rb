c0ed5aa729db8a5a2837f42df07117032046565f
halki <s.halki0203@icloud.com>
2020-07-12 22:43:27 +0900

    up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
-  endr
+  end
 
   def create
     @user = User.new(user_params)
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
     #  redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
     end
   end
 
